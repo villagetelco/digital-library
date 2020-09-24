95e549bc37f6fa0043f0274021601642ed823a23
tgillett <tgillett@gmail.com>
2020-09-24 21:18:39 +1000

    Update library memory devices linking

--- diglib-build/files/etc/init.d/set-rachel.sh
+++ diglib-build/files/etc/init.d/set-rachel.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 # Wait for memory devices to be available
-sleep 10
+sleep 5
 
 # ------------------------------
 
 # Mount the memory device in case auto mount did not work.
-# /dev/sda is primary USB and /dev/sdb is secondary USB 
+# /dev/sda is primary and /dev/sdb is secondary. 
 
 # Make dirs in case it is first boot
 mkdir -p /mnt/sda1
@@ -31,18 +31,19 @@ fi
 rm /www/rachel
 rm /www/rachel-local2
 
-#Set up default Rachel directory in case there are no memory devices installed
+#Set up default Library directory in case there are no memory devices installed
 ln -s -f /www/rachel-x /www/rachel
 
-# Find library memory device and link to it
-# Check for primary USB library on "sda1"
-if [ -e "/mnt/sda1/##LIBRARY##" ]; then
+# Find library memory devices and link.
+# Check for primary memory device on "sda1" to use for main library.
+if [ -e "/dev/sda1" ]; then
 	rm /www/rachel
 	ln -s -f /mnt/sda1	/www/rachel
+fi
 
-	if [ -e "/dev/sdb1" ]; then # Check for secondary USB device to use for additional local content
-		ln -s -f /mnt/sdb1 /www/rachel-local2
-	fi
+# Check for secondary memory device on "sdb1" to use for additional local content.
+if [ -e "/dev/sdb1" ]; then 
+	ln -s -f /mnt/sdb1 /www/rachel-local2
 fi
 
 exit
