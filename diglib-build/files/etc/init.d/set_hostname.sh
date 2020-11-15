92733c8ba49277fdbf8ea953a366cc161d04cfb9
tgillett <tgillett@gmail.com>
2020-11-16 07:26:01 +1000

    Add hostname to /etc/hosts for fast resolve

--- diglib-build/files/etc/init.d/set_hostname.sh
+++ diglib-build/files/etc/init.d/set_hostname.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-HOST="Digital-Library"
+HOSTNAME="Digital-Library"
+ipaddr=$(uci -q get network.lan.ipaddr)
 
 # Set the hostname
-uci set system.@system[0].hostname=$HOST
+uci set system.@system[0].hostname=$HOSTNAME
 uci commit system
 
 # Set the hostname as the Common Name in the SSL certificate for the web server.
-uci set uhttpd.px5g.commonname=$HOST
+uci set uhttpd.px5g.commonname=$HOSTNAME
 uci commit uhttpd
 
 # Set the system hostname
 echo $(uci get system.@system[0].hostname) > /proc/sys/kernel/hostname
 
+# Put hostname in hosts file to ensure it can be resolved.
+sed -i '/$HOSTNAME/d' /etc/hosts
+echo "$ipaddr $HOSTNAME" >> /etc/hosts
+
 # Restart dnsmasq to get hostname
 /etc/init.d/dnsmasq restart
-
