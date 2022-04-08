60e71f4400b31a3db84e07175bc377c645123b40
tgillett <tgillett@gmail.com>
2022-04-08 14:18:23 +1000

    Add support for USB firmware update

--- diglib-build/files/etc/init.d/set-rachel.sh
+++ diglib-build/files/etc/init.d/set-rachel.sh
@@ -12,6 +12,7 @@ mkdir -p /mnt/sda1
 mkdir -p /mnt/sdb1
 mkdir -p /mnt/sdc1
 mkdir -p /mnt/sdd1
+mkdir -p /mnt/mmcblk0p3
 
 if [ -e "/dev/sda1" ]; then
 mount -rw /dev/sda1  /mnt/sda1
@@ -25,43 +26,58 @@ fi
 if [ -e "/dev/sdd1" ]; then
 mount -rw /dev/sdd1  /mnt/sdd1
 fi
+if [ -e "/dev/mmcblk0p3" ]; then
+mount -rw /dev/mmcblk0p3  /mnt/mmcblk0p3
+fi
 
 # Remove old links
-rm /www/rachel
-rm /www/rachel-local2
+rm /www/library
+rm /www/library-local2
 
 # Find Library memory devices and link.
 # Case 1. Check for a primary Library memory device on "sda1" to use for main library.
 if [ -e "/dev/sda1" ] && [ -e "/mnt/sda1/##LIBRARY##" ]; then
-	rm /www/rachel
-	ln -s -f /mnt/sda1	/www/rachel
+	rm /www/library
+	ln -s -f /mnt/sda1	/www/library
 	mkdir -p /mnt/sda1/temp # Make temp directory for uploading
 	# Check for secondary memory device on "sdb1" to use for additional local content.
 	if [ -e "/dev/sdb1" ]; then 
-		ln -s -f /mnt/sdb1 /www/rachel-local2
+		ln -s -f /mnt/sdb1 /www/library-local2
 	fi
 # Case 2. Check for primary Library memory device on "sdb1" to use for main library.
 elif [ -e "/dev/sdb1" ] && [ -e "/mnt/sdb1/##LIBRARY##" ]; then
-	rm /www/rachel
-	ln -s -f /mnt/sdb1	/www/rachel
+	rm /www/library
+	ln -s -f /mnt/sdb1	/www/library
 		mkdir -p /mnt/sdb1/temp # Make temp directory for uploading
 	# Check for secondary memory device on "sda1" to use for additional local content.
 	if [ -e "/dev/sda1" ]; then 
-		ln -s -f /mnt/sda1 /www/rachel-local2
+		ln -s -f /mnt/sda1 /www/library-local2
 	fi
-# Case 3. Check for non-Library memory device on "sda1" to use for main library.
+# Case 3. Check for primary Library memory device on "mmcblk0p3" (for RPi) to use for main library.
+elif [ -e "/dev/mmcblk0p3" ] && [ -e "/mnt/mmcblk0p3/##LIBRARY##" ]; then
+	rm /www/library
+	ln -s -f /mnt/mmcblk0p3 /www/library
+	mkdir -p /mnt/mmcblk0p3/temp # Make temp directory for uploading
+	# Check for secondary memory device on "sdb1" to use for additional local content.
+	if [ -e "/dev/sda1" ]; then 
+		ln -s -f /mnt/sda1 /www/library-local2
+	fi
+# Case 4. Check for non-Library memory device on "sda1" to use for main library.
 elif [ -e "/dev/sda1" ]; then
-	rm /www/rachel
-	ln -s -f /mnt/sda1	/www/rachel
+	rm /www/library
+	ln -s -f /mnt/sda1	/www/library
 	mkdir -p /mnt/sda1/temp # Make temp directory for uploading
 	# Check for secondary memory device on "sdb1" to use for additional local content.
 	if [ -e "/dev/sdb1" ]; then 
-		ln -s -f /mnt/sdb1 /www/rachel-local2
+		ln -s -f /mnt/sdb1 /www/library-local2
 	fi
-# Case 4. No memory device on "sda1" so so just use the default home page.
+# Case 5. No memory device on "sda1" or "mmcblk0p3" so so just use the default home page.
 else
-	rm /www/rachel
-	ln -s -f /www/rachel-x /www/rachel
+	rm /www/library
+	ln -s -f /www/library-x /www/library
+	if [ -e "/dev/sdb1" ]; then 
+		ln -s -f /mnt/sdb1 /www/library-local2
+	fi
 fi
 
 exit
