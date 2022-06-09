f6d70a4ec112463985214e0b7a7b26d1d86297f3
tgillett <tgillett@gmail.com>
2022-06-09 12:32:19 +1000

    Update cache purge code

--- diglib-build/files/bin/purge_cache.sh
+++ diglib-build/files/bin/purge_cache.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 # Purge the on-disk cache
-sleep 1
-polipo -c /tmp/etc/polipo.conf -x
+		polipo-pid=$(cat /var/run/polipo.pid)
+		kill -USR1 polipo-pid
+		sleep 1
+		polipo -c /tmp/etc/polipo.conf -x > /dev/null
+		kill -USR2 polipo-pid
 
 
